a2212bbb295a5c68cefbed02cfc53591b44a9131
DragonsKiller <dragon.airos@yandex.by>
2018-06-14 14:39:27 +0300

    added CRUD idea and has_many relation between trader and ideas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180614104111) do
+ActiveRecord::Schema.define(version: 20180614113359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,22 @@ ActiveRecord::Schema.define(version: 20180614104111) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "ideas", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.text "problem"
+    t.string "industry"
+    t.string "team"
+    t.string "geographical_focus"
+    t.string "requirements"
+    t.text "next_steps"
+    t.date "publication_end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "trader_id"
+    t.index ["trader_id"], name: "index_ideas_on_trader_id"
   end
 
   create_table "investors", force: :cascade do |t|
@@ -73,4 +89,5 @@ ActiveRecord::Schema.define(version: 20180614104111) do
     t.index ["reset_password_token"], name: "index_traders_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "ideas", "traders"
 end
