984670c63c40b9c334ca4411642ce65d7cb3d94c
DragonsKiller <dragon.airos@yandex.by>
2018-06-28 13:18:58 +0300

    changed diliver_now to deliver_later(wait: 1.seconds)

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -61,7 +61,7 @@ class IdeasController < ApplicationController
     end
   end
 
-  private
+  private 
     # Use callbacks to share common setup or constraints between actions.
     def set_idea
       @idea = Idea.find(params[:id])
