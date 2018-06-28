7fe3452ace49f7f1c909ca5c81644cf53379447d
DragonsKiller <dragon.airos@yandex.by>
2018-06-28 12:16:28 +0300

    Revert "Interf"

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
+  resources :ideas
   devise_for :investors
   devise_for :traders
   devise_for :admins
-
-  namespace :api do
-    namespace :v1 do
-      resources :ideas
-    end
-  end
 
   root 'ideas#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
